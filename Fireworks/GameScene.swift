a561a66cd4b1688f11c6fbd64c53730dd539024d
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-12-01 23:17:35 -0300

    feat: Initial commit

---
+++ Fireworks/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Fireworks
+//
+//  Created by Rodrigo on 01-12-24.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
